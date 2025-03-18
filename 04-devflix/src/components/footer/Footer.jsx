4316b77a88c4788a1c7782d76861c42d5ed9d50d
Livia Ormeleze <liviasenai.ormeleze@gmail.com>
2025-03-18 16:40:54 -0300

    100% concertado o footer

--- 04-devflix/src/components/footer/Footer.jsx
+++ 04-devflix/src/components/footer/Footer.jsx
@@ -4,7 +4,7 @@ const Footer = (props) => {
   return (
     <footer>
     <p>Feito com ❤️ por 
-        <a href={props.devLink}>{props.devName}</a>
+        <a href={props.devLink}> {props.devName}</a>
         </p>
   </footer>
   )
