9215a7315577ee1bf3558d6407b1871cf4ac324f
Livia Ormeleze <liviasenai.ormeleze@gmail.com>
2025-04-22 10:56:26 -0300

    iniciado o item de promoção

---
+++ 07-devsteam/src/components/Promotion.jsx
@@ -0,0 +1,14 @@
+import React from "react";
+
+const Promotion = () => {
+  return (
+  <div id="promotion">
+    <h2>Promoções</h2>
+    <div id="itensPromo">
+     {/* inserrir os cards de promocão */}
+    </div>
+  </div>
+  )
+};
+
+export default Promotion;
